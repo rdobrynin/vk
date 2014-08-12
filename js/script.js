9974f6352d284fd329cf9640edd958bb38e6b074
romdo <roman.d@brilliant-solutions.eu>
2014-08-12 12:46:49 +0300

    fixed balance div

--- js/script.js
+++ js/script.js
@@ -10,13 +10,18 @@ $(function () {
     str = $('#balance-amount').html();
     var letters = str.length;
 
+    if(letters == 1) {
+        $('.green-plus-wrapper').css({ 'right': '-23px' });
+        $('.green-plus-wrapper').css({ 'top': '-55px' });
+    }
+
     if (letters == 2) {
         $('.coin-balance-big').css({ 'width': '66px' });
         $('.coin-balance-bg').css({ 'width': '75px' });
         $('.coin-balance-wrapper').css({ 'width': '75px' });
         $('.coin-balance-wrapper').css({ 'left': '720px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '20px' });
+        $('.green-plus-wrapper').css({ 'top': '-15px' });
     }
     else if (letters == 3) {
         $('.coin-balance-big').css({ 'width': '78px' });
@@ -24,7 +29,7 @@ $(function () {
         $('.coin-balance-wrapper').css({ 'width': '86px' });
         $('.coin-balance-wrapper').css({ 'left': '713px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-15px' });
     }
     else if (letters == 4) {
         $('.coin-balance-big').css({ 'width': '103px' });
@@ -32,7 +37,7 @@ $(function () {
         $('.coin-balance-wrapper').css({ 'width': '109px' });
         $('.coin-balance-wrapper').css({ 'left': '688px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-15px' });
     }
     else if (letters == 5) {
         $('.coin-balance-big').css({ 'width': '114px' });
@@ -40,7 +45,7 @@ $(function () {
         $('.coin-balance-wrapper').css({ 'width': '134px' });
         $('.coin-balance-wrapper').css({ 'left': '674px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-15px' });
     }
     else if (letters == 6) {
         $('.coin-balance-big').css({ 'width': '131px' });
@@ -48,57 +53,62 @@ $(function () {
         $('.coin-balance-wrapper').css({ 'width': '150px' });
         $('.coin-balance-wrapper').css({ 'left': '658px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-15px' });
     }
     else if (letters == 7) {
         $('.balance-amount').css({ 'font-size': '18.5px' });
-        $('.balance-amount').css({ 'margin-top': '4.5px' });
+        $('.balance-amount').css({ 'top': '0' });
         $('.coin-balance-big').css({ 'width': '131px' });
         $('.coin-balance-bg').css({ 'width': '139px' });
         $('.coin-balance-wrapper').css({ 'width': '150px' });
         $('.coin-balance-wrapper').css({ 'left': '658px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-9px' });
+        $('.coin-gold-icon').css({ 'top': '-30px' });
     }
     else if (letters == 8) {
         $('.balance-amount').css({ 'font-size': '17px' });
-        $('.balance-amount').css({ 'margin-top': '5.5px' });
+        $('.balance-amount').css({ 'top': '2px' });
         $('.coin-balance-big').css({ 'width': '131px' });
         $('.coin-balance-bg').css({ 'width': '139px' });
         $('.coin-balance-wrapper').css({ 'width': '150px' });
         $('.coin-balance-wrapper').css({ 'left': '658px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-6px' });
+        $('.coin-gold-icon').css({ 'top': '-27px' });
     }
     else if (letters == 9) {
         $('.balance-amount').css({ 'font-size': '16px' });
-        $('.balance-amount').css({ 'margin-top': '6.5px' });
+        $('.balance-amount').css({ 'top': '2px' });
         $('.coin-balance-big').css({ 'width': '131px' });
         $('.coin-balance-bg').css({ 'width': '139px' });
         $('.coin-balance-wrapper').css({ 'width': '150px' });
         $('.coin-balance-wrapper').css({ 'left': '658px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-4px' });
+        $('.coin-gold-icon').css({ 'top': '-25px' });
     }
     else if (letters == 10) {
         $('.balance-amount').css({ 'font-size': '13px' });
-        $('.balance-amount').css({ 'margin-top': '7.5px' });
+        $('.balance-amount').css({ 'top': '3.5px' });
         $('.coin-balance-big').css({ 'width': '131px' });
         $('.coin-balance-bg').css({ 'width': '139px' });
         $('.coin-balance-wrapper').css({ 'width': '150px' });
         $('.coin-balance-wrapper').css({ 'left': '658px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-3px' });
+        $('.coin-gold-icon').css({ 'top': '-23px' });
     }
     else if (letters == 11) {
         $('.balance-amount').css({ 'font-size': '12.1px' });
-        $('.balance-amount').css({ 'margin-top': '8.5px' });
+        $('.balance-amount').css({ 'top': '3.5px' });
         $('.coin-balance-big').css({ 'width': '131px' });
         $('.coin-balance-bg').css({ 'width': '139px' });
         $('.coin-balance-wrapper').css({ 'width': '150px' });
         $('.coin-balance-wrapper').css({ 'left': '658px' });
         $('.green-plus-wrapper').css({ 'right': '-60px' });
-        $('.green-plus-wrapper').css({ 'top': '18px' });
+        $('.green-plus-wrapper').css({ 'top': '-2px' });
+        $('.coin-gold-icon').css({ 'top': '-23px' });
     }
     else {
 
