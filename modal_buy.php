3e8de0c0b7c8edd47c93b2df1c2f5bc089cf4925
romdo <roman.d@brilliant-solutions.eu>
2014-08-19 14:28:36 +0300

    fixed css

--- modal_buy.php
+++ modal_buy.php
@@ -13,52 +13,66 @@
                 <div class="modal-row-bg">
                     <div class="modal-row row-300">
                         <span class="coins_cost coin_300">300</span>
+                        <div class="button-wrapper-buy">
+                            <button class="btn btn-riu-modal-primary">3 голоса</button>
+                        </div>
                     </div>
                 </div>
             </div>
-
             <!--            ROW 2-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-1500">
                         <span class="coins_cost coin_1500">1500</span>
+                        <div class="button-wrapper-buy">
+                            <button class="btn btn-riu-modal-primary">10 голосов</button>
+                        </div>
                     </div>
                 </div>
             </div>
-
             <!--            ROW 3-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-3200">
                         <span class="coins_cost coin_3200">3200</span>
+                        <div class="button-wrapper-buy">
+                            <button class="btn btn-riu-modal-primary">20 голосов</button>
+                        </div>
                     </div>
                 </div>
             </div>
-
             <!--            ROW 4-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-7500">
                         <span class="coins_cost coin_7500">7500</span>
+                        <div class="button-wrapper-buy">
+                            <button class="btn btn-riu-modal-primary">30 голосов</button>
+                        </div>
                     </div>
                 </div>
             </div>
-
             <!--            ROW 5-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-15000 coin_15000">
                         <span class="coins_cost coin_15000">15000</span>
+                        <div class="button-wrapper-buy">
+                            <button class="btn btn-riu-modal-primary" style="top: -14px;">50 голосов</button>
+                        </div>
                     </div>
                 </div>
             </div>
-
             <!--            ROW 5-->
             <div class="modal-row-wrapper" style="margin-top: 15px">
                 <div class="modal-row-bg">
                     <div class="modal-row row-gift">
                         <div class="present-row-gift"><img id="gold-gift" src="images/gold_gift@2x.png" alt="" width="90" height="94"/></div>
                     </div>
+                    <div class="button-wrapper-buy" style="margin-top: 2px;">
+                        <button class="btn btn-riu-modal-success">получить</button>
+                    </div>
+
                 </div>
             </div>
         </div>
