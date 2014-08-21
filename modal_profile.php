8f487ba810315b2b184988146762b3e5d4197d81
romdo <roman.d@brilliant-solutions.eu>
2014-08-21 14:33:15 +0300

    added title to pofile

--- modal_profile.php
+++ modal_profile.php
@@ -6,15 +6,28 @@
             <div class="top-profile">
                 <div class="place-profile-wrapper">
                     <div class="bg-place-profile">
-                        <div class="place-profile"></div>
+                        <div class="place-profile">
+                            <!--ICON DIV-->
+                            <span id="modal_profile_place_1" class="hide"><img src="images/modal_1_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_2" class="hide"><img src="images/modal_2_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_3" class="hide"><img src="images/modal_3_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_4" class="hide"><img src="images/modal_4_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_5" class="hide"><img src="images/modal_5_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_6" class="hide"><img src="images/modal_6_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_7" class="hide"><img src="images/modal_7_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_8" class="hide"><img src="images/modal_8_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_9" class=""><img src="images/modal_9_profile@2x.png" alt="" width="40" height="41"/></span>
+                            <span id="modal_profile_place_10" class="hide"><img src="images/modal_10_profile@2x.png" alt="" width="40" height="41"/></span>
+                        </div>
                     </div>
                 </div>
+                <div class="profile-title">александра, 22</div>
                 <div class="close-modal-wrapper-profile">
                     <button type="button" class=" btn btn-close btn-close-profile" data-dismiss="modal">
                         <img id="modal-profile-close" src="images/close_profile.png" alt="" width="23" height="20"/>
                     </button>
                 </div>
-            </div>
+            </div><!--            end top-profile-->
             <div class="inside-bg-profile">
                 <div class="bg-profile-wrapper">
                     <div class="content-profile-wrapper">
