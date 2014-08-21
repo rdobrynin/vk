22d6b12f86c84a1e1aa48fcb85bd78f93125cd18
romdo <roman.d@brilliant-solutions.eu>
2014-08-21 17:31:25 +0300

    reformed code

--- modal_buy.php
+++ modal_buy.php
@@ -2,7 +2,7 @@
 <div class="modal fade" id="modal-buy" tabindex="-1" role="dialog" aria-labelledby="modal-buy" data-backdrop="static" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-buy">
-<!--            <span class="modal-title">Докупить<br/>монеты</span>-->
+<!--<span class="modal-title">Докупить<br/>монеты</span>-->
 
             <div class="text-buy-coins"><img id="text-buy-coins" src="images/text_buy_money.png" alt="" width="168" height="59"/></div>
 
@@ -11,7 +11,7 @@
            <div class="close-modal-wrapper-buy">
                <button type="button" class=" btn btn-close" data-dismiss="modal"> <img id="modal-buy-close" src="images/modal_close.png" alt="" width="21" height="20"/></button>
            </div>
-<!--            ROW 1-->
+<!--ROW 1-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-300">
@@ -22,7 +22,7 @@
                     </div>
                 </div>
             </div>
-            <!--            ROW 2-->
+            <!--ROW 2-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-1500">
@@ -33,7 +33,7 @@
                     </div>
                 </div>
             </div>
-            <!--            ROW 3-->
+            <!--ROW 3-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-3200">
@@ -44,7 +44,7 @@
                     </div>
                 </div>
             </div>
-            <!--            ROW 4-->
+            <!--ROW 4-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-7500">
@@ -55,7 +55,7 @@
                     </div>
                 </div>
             </div>
-            <!--            ROW 5-->
+            <!--ROW 5-->
             <div class="modal-row-wrapper">
                 <div class="modal-row-bg">
                     <div class="modal-row row-15000 coin_15000">
@@ -66,7 +66,7 @@
                     </div>
                 </div>
             </div>
-            <!--            ROW 5-->
+            <!--ROW 6-->
             <div class="modal-row-wrapper" style="margin-top: 15px; margin-bottom:20px;">
                 <div class="modal-row-bg">
                     <div class="modal-row row-gift">
