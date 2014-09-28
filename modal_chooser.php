14af841b62c597e1f252e04597999f532dac6a64
rdobrynin <roman.dobrynin@gmail.com>
2014-09-29 00:21:50 +0300

    added modal

---
+++ modal_chooser.php
@@ -0,0 +1,22 @@
+<!-- Modal -->
+<div class="modal fade" id="modal-chooser" tabindex="-1" role="dialog" aria-labelledby="modal-chooserLabel" aria-hidden="true">
+  <div class="chooser-static">
+  <div class="modal-dialog">
+    <div class="modal-content">
+  <div class="modal-chooser">
+<div id="chooser-sub-modal-1">
+    <div class="user-frame-modal">
+      <div class="photo">
+        <img src="images/photo_top_1@2x.jpg" alt="" width="59" height="58">
+      </div>
+    </div>
+    <p><b>Вы первый игрок за столом</b></p>
+    <p>Пока вы ожидаете других игроков, <br />не желаете ли попрактиковаться в рисовании?</p>
+    <button class="btn btn-riu-modal-primary" id="btn-begin-draw" style="width: auto; margin-top: 20px; margin-right: 8px;">Начать рисовать</button>
+    <button class="btn btn-riu-modal-success green" id="btn-wait" style="width: auto; margin-top: 20px; margin-left: 8px;">Просто подождать</button>
+</div>
+  </div>
+    </div>
+  </div>
+    </div>
+</div>
