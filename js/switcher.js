3d7afd4e0aa79c53fbb65d6eec096425904c3b9f
romdo <roman.d@brilliant-solutions.eu>
2014-10-28 10:10:35 +0200

    fixed switcher

--- js/switcher.js
+++ js/switcher.js
@@ -5,7 +5,9 @@
 function switcher(param) {
     var current_obj = $('#switcher').attr('class');
     var current = current_obj.split(' ')[1];
+    var new_param = current.substring(4, current.length);
     if(param != 'current') {
     $('#switcher').removeClass(current).addClass('btn-'+param);
+
     }
 }
